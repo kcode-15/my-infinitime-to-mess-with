a0065c31d58b279076beeadd22f07f809334d98e
kcode-15 <eugeniadesampayo+kcode@gmail.com>
2026-09-06 07:42:01 +1000

    Add ActivityApp class for activity management

---
+++ src/displayapp/screens/ActivityApp.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "displayapp/apps/Apps.h"
+#include "displayapp/screens/Screen.h"
+#include "displayapp/Controllers.h"
+
+namespace Pinetime {
+  namespace Applications {
+    namespace Screens {
+
+      class ActivityApp : public Screen {
+        public:
+          ActivityApp();
+          ~ActivityApp() override;
+
+        private:
+          lv_obj_t* textLabel;
+          lv_obj_t* actionButton;
+          
+          // This tells the watch what to do when the button is clicked
+          static void ButtonEventHandler(lv_obj_t* obj, lv_event_t event);
+          void PickRandomActivity();
+      };
+    }
+
+    // This registers your app so the watch menu knows what icon to use
+    template <>
+    struct AppTraits<Apps::ActivityApp> {
+      static constexpr Apps app = Apps::ActivityApp;
+      static constexpr const char* icon = "\uF04B"; // Just a standard play icon
+      static Screens::Screen* Create(AppControllers& controllers) {
+        return new Screens::Screen(controllers);
+      }
+    };
+  }
+}
